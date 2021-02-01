88a086967459afcb7df0a2ddc15f99b40b5508fd
zainullin <radm-z@yandex.ru>
2021-02-01 16:54:23 +0300

    Добавьте файлы проекта.

---
+++ ClientApplication/Client.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Xml;
+
+namespace ClientApplication
+{
+    class Client
+    {
+        private static float latency; // in ms
+
+        private static float average = 0.0F;
+        private static float dispersion = 0.0F;
+        private static float mode = 0.0F;
+        private static float median = 0.0F;
+        private static float eta = 0.005F;
+        private static ulong n = 0;
+        private static int stockCost = 0;
+
+
+
+        private static UdpClient udpClient;
+        private static int port;
+        private static MulticastOption multicastOption;
+
+        private static IPAddress multicastAddress;
+        private static IPAddress localAddress;
+        private static IPEndPoint localEndPoint;
+        private static IPEndPoint remoteEndPoint;
+
+        private static string configUri = Directory.GetCurrentDirectory() + "./../../clientConfigs/config.xml";
+
+
+        public static void LoadConfiguration()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(configUri);
+
+            XmlNode latencySettings = xmldoc.SelectSingleNode("configuration/latencySettings");
+            latency = Convert.ToSingle(latencySettings.SelectSingleNode("latency").InnerText);
+
+            XmlNode multiCastSettings = xmldoc.SelectSingleNode("configuration/multiCastSettings");
+            multicastAddress = IPAddress.Parse(multiCastSettings.SelectSingleNode("multicastAddress").InnerText);
+            localAddress = IPAddress.Any;
+            port = Convert.ToInt32(multiCastSettings.SelectSingleNode("port").InnerText);
+        }
+        public static void ConfigureSocket()
+        {
+            remoteEndPoint = new IPEndPoint(multicastAddress, port);
+            localEndPoint = new IPEndPoint(localAddress, port);
+
+            // Create and configure UdpClient
+            udpClient = new UdpClient();
+            udpClient.ExclusiveAddressUse = false;
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.ExclusiveAddressUse = false;
+            // Bind, Join
+            udpClient.Client.Bind(localEndPoint);
+            udpClient.JoinMulticastGroup(multicastAddress, localAddress);
+
+            //Configure socket
+            udpClient.Client.SetIPProtectionLevel(IPProtectionLevel.Restricted);
+            udpClient.Client.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 1);
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Udp, SocketOptionName.NoChecksum, true);
+            //Configure multicast group
+            multicastOption = new MulticastOption(multicastAddress);
+            udpClient.Client.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, multicastOption);
+        }
+        static float calcAverage(float nextValue)
+        {
+            return average + (nextValue - average) / n;
+        }
+        static float calcMedian(float nextValue)
+        {
+            return median + eta * Math.Sign(nextValue - median);
+        }
+        static void calcStatistics()
+        {
+            average = calcAverage(1);
+        }
+        static void Receive()
+        {
+            while (true)
+            {
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = udpClient.Receive(ref ipEndPoint);
+                stockCost = BitConverter.ToInt32(data,0);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int stockCost;
+            LoadConfiguration();
+            ConfigureSocket();
+            Thread calculate = new Thread(Receive);
+            calcStatistics();
+        }
+    }
+}
---
+++ ServerApplication/Server.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerApplication
+{
+    class Server
+    {
+        
+        private static int minRange;
+        private static int maxRange;
+        private static Random random = new Random();
+
+        private static IPAddress serverAddress;
+        private static IPEndPoint endPoint;
+        private static int port;
+        private static Socket socket;
+        private static MulticastOption multicastOption;
+
+        private static string configUri = Directory.GetCurrentDirectory() + "./../../serverConfigs/config.xml";
+
+
+        public static void LoadConfiguration()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(configUri);
+
+            XmlNode randomSettings = xmldoc.SelectSingleNode("configuration/randomSettings");
+            minRange = Convert.ToInt32(randomSettings.SelectSingleNode("minRange").InnerText);
+            maxRange = Convert.ToInt32(randomSettings.SelectSingleNode("maxRange").InnerText);
+
+            XmlNode multiCastSettings = xmldoc.SelectSingleNode("configuration/multiCastSettings");
+            serverAddress = IPAddress.Parse(multiCastSettings.SelectSingleNode("serverAddress").InnerText);
+            port = Convert.ToInt32(multiCastSettings.SelectSingleNode("port").InnerText);
+        }
+        public static void ConfigureSocket()
+        {
+            socket = new Socket(AddressFamily.InterNetwork,
+                                     SocketType.Dgram,
+                                     ProtocolType.Udp);
+            endPoint = new IPEndPoint(serverAddress, port);
+            //Configure socket
+            socket.SetIPProtectionLevel(IPProtectionLevel.Restricted);
+            socket.SetSocketOption(SocketOptionLevel.Udp, SocketOptionName.NoDelay, true);
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 1);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.SendTimeout, 0);
+            socket.SetSocketOption(SocketOptionLevel.Udp, SocketOptionName.NoChecksum, true);
+            //Configure multicast group
+            multicastOption = new MulticastOption(serverAddress);
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, multicastOption);
+        }
+        static int GetStockCost()
+        {
+            return random.Next(minRange,maxRange);
+        }
+        static void Main(string[] args)
+        {
+            int stockCost;
+            LoadConfiguration();
+            ConfigureSocket();
+            Console.WriteLine("Server succesfully started");
+            Console.WriteLine("Press CTRL + C to exit ");
+            byte[] buffer = new byte[sizeof(int)+ sizeof(ulong)];
+            byte[] stock, count_buf;
+            int count = 0;
+            while (true)
+            {
+                stockCost = GetStockCost();
+                count += 1;
+                stock = BitConverter.GetBytes(stockCost);
+                count_buf = BitConverter.GetBytes(count);
+                Buffer.BlockCopy(stock, 0, buffer, 0, sizeof(int));
+                Buffer.BlockCopy(count_buf, 0, buffer, sizeof(int), sizeof(int));
+                socket.SendTo(buffer,SocketFlags.None,endPoint);
+            }
+        }
+    }
+}
